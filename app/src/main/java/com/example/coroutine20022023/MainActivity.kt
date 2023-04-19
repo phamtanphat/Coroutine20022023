fdea6f1a8734f0db27170a58727eefd2af5e2865
Phat <phatdroid94@gmail.com>
2023-04-19 20:49:40 +0700

    async

--- app/src/main/java/com/example/coroutine20022023/MainActivity.kt
+++ app/src/main/java/com/example/coroutine20022023/MainActivity.kt
@@ -4,32 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.coroutines.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        CoroutineScope(Dispatchers.IO).launch {
+            var a = async { randomA() }
 
-        CoroutineScope(CoroutineName("name1") + Dispatchers.IO + Job()).launch {
-            val startTime = System.currentTimeMillis()
-            val job = launch(Dispatchers.Default) {
-                var nextPrintTime = startTime
-                var i = 0
-                while (i < 5) { // computation loop, just wastes CPU
-                    // print a message twice a second
-                    if (isActive) {
-                        if (System.currentTimeMillis() >= nextPrintTime) {
-                            Log.d("BBB","job: I'm sleeping ${i++} ...")
-                            nextPrintTime += 500L
-                        }
-                    }
-                }
-            }
-            delay(1300L) // delay a bit
-            Log.d("BBB","main: I'm tired of waiting!")
-            job.cancelAndJoin() // cancels the job and waits for its completion
-            Log.d("BBB","main: Now I can quit.")
+            var b = async { randomB() }
+
+            var c = a.await() + b.await()
+
+            Log.d("BBB", a.await().toString())
+            Log.d("BBB", b.await().toString())
+            Log.d("BBB", c.toString())
         }
+    }
+
+    suspend fun randomA(): Int {
+        delay(1000)
+        return Random.nextInt(10)
+    }
+
+    suspend fun randomB(): Int {
+        delay(1500)
+        return Random.nextInt(10)
     }
 }
