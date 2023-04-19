f3d6ed1261e88d784b5c7dd25bc5d7db9348a1c3
Phat <phatdroid94@gmail.com>
2023-04-19 20:05:04 +0700

    join

--- app/src/main/java/com/example/coroutine20022023/MainActivity.kt
+++ app/src/main/java/com/example/coroutine20022023/MainActivity.kt
@@ -3,14 +3,35 @@ package com.example.coroutine20022023
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        for (element in 1..1_000_000L) {
-            Log.d("BBB", element.toString())
+
+        CoroutineScope(CoroutineName("name1") + Dispatchers.IO + Job()).launch {
+            Log.d("BBB", "Coroutine parent ${Thread.currentThread().name}")
+
+            val job = launch {
+                Log.d("BBB", "Coroutine 1 ${Thread.currentThread().name}")
+                for (element in 1..1000) {
+                    Log.d("BBB", "Coroutine 1 $element")
+                    delay(50)
+                }
+            }
+
+            job.join()
+            val job2 = launch {
+                Log.d("BBB", "Coroutine 2 ${Thread.currentThread().name}")
+                for (element in 1..1000) {
+                    Log.d("BBB", "Coroutine 2 $element")
+                    delay(50)
+                }
+            }
+
+            Log.d("BBB", "Job $job")
         }
     }
 }
