0c316b5917f9649c1b8c85f495ba12ab66a0b945
Phat <phatdroid94@gmail.com>
2023-04-24 20:54:52 +0700

    with context

--- app/src/main/java/com/example/coroutine20022023/MainActivity.kt
+++ app/src/main/java/com/example/coroutine20022023/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.coroutine20022023
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.annotation.MainThread
 import androidx.recyclerview.widget.RecyclerView.ViewCacheExtension
 import kotlinx.coroutines.*
 import kotlin.random.Random
@@ -13,39 +15,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         CoroutineScope(Dispatchers.IO).launch {
-            val handler = CoroutineExceptionHandler { _, throwable ->
-                Log.d("BBB", throwable.toString())
+            for (element in 1..10) {
+                delay(100)
             }
 
-            val job = CoroutineScope(Dispatchers.Default + handler).launch {
-                launch {
-                    delay(200)
-                    throw NullPointerException()
-                }
-
-                launch {
-                    delay(100)
-                    throw ArithmeticException()
-                }
-
-                launch {
-                    delay(500)
-                    throw NumberFormatException()
-                }
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, "Finish", Toast.LENGTH_SHORT).show()
             }
-
-            delay(1000)
-            job.join()
         }
     }
 
-    suspend fun randomA(): Int {
-        delay(1000)
-        throw java.lang.NullPointerException()
-    }
-
-    suspend fun randomB(): Int {
-        delay(1500)
-        return Random.nextInt(10)
-    }
 }
